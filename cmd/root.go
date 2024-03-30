fb823b0da0bbd0d902c7904f5bbdcf38210be809
Evan Johnson <evan@runreveal.com>
2024-03-30 09:07:43 -0500

    Add "add" subcommand.

--- cmd/root.go
+++ cmd/root.go
@@ -5,8 +5,10 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
@@ -36,6 +38,15 @@ var listCmd = &cobra.Command{
 	},
 }
 
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add file to prompt list",
+	Long:  `add file to prompt list`,
+	Run: func(cmd *cobra.Command, args []string) {
+		addPrompt(cmd, args)
+	},
+}
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -57,6 +68,7 @@ func init() {
 	openAIToken = token
 
 	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(addCmd)
 }
 
 func listPrompts() {
@@ -86,7 +98,7 @@ func run(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatalf("could not prompt: %s\n", err)
 	}
-	println(resp)
+	fmt.Println(resp)
 
 }
 
@@ -110,4 +122,36 @@ func Prompt(query string) (string, error) {
 	}
 	return resp.Choices[0].Message.Content, nil
 
+}
+
+func addPrompt(cmd *cobra.Command, args []string) {
+	if len(args) < 1 {
+		cmd.Help()
+		return
+	}
+	promptName := args[0]
+
+	// Get file name, strip off path
+	fileParts := strings.Split(promptName, "/")
+	fname := fileParts[len(fileParts)-1]
+
+	// Check if this prompt already exists
+	var f = promptDir + "/" + fname
+	_, err := os.Stat(f)
+	if err == nil {
+		log.Fatalf("prompt %s already exists\n", f)
+	}
+
+	// Read the contents
+	buf, err := os.ReadFile(promptName)
+	if err != nil {
+		log.Fatalf("could not read new prompt %s: %s\n", f, err)
+	}
+
+	// Write to the prompt dir
+	err = os.WriteFile(f, buf, 0644)
+	if err != nil {
+		log.Fatalf("could not write new prompt %s: %s\n", f, err)
+	}
+	println("added prompt", f)
 }
